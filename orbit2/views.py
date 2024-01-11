3671dd72dace855ad6ae9aea102071343cac4621
Abhishek Rajput <abhishek620380@gmail.com>
2024-01-11 20:55:05 +0530

    Initial Commit

--- orbit2/views.py
+++ orbit2/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse , redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Count
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_protect
+from django.utils import timezone
+from django.db import IntegrityError
+import csv
+import os
+from io import BytesIO
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+import uuid
+from django.core.files.storage import default_storage
+from django.conf import settings
+import os
+from io import BytesIO
+from django.db.models import Q
+from django.utils.timezone import localtime, make_aware
 
-# Create your views here.
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request , username=username , password=password)
+        if user is not None:
+            login(request , user)
+            return redirect('login')
+        else:
+            return render(request , 'login.html' , {'error': "Invalid Username or Password"})
+    return render(request , 'login.html')
+
