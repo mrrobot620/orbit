f131fbeedf957a4186598951e891011d4735da2e
Abhishek Rajput <abhishek620380@gmail.com>
2024-01-12 11:03:39 +0530

    Add Orphan Page

--- orbit2/urls.py
+++ orbit2/urls.py
@@ -3,5 +3,8 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path("login" , views.login , name="login")
+    path("login" , views.login , name="login"),
+    path("home" , views.home , name="home"),
+    path("add_orphan" , views.add_orphan_page , name="add_orphan"),
+    path('classify_image/', views.classify_image, name='classify_image')
 ]
--- orbit2/views.py
+++ orbit2/views.py
@@ -9,6 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_protect
 from django.utils import timezone
 from django.db import IntegrityError
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 import csv
 import os
 from io import BytesIO
@@ -23,6 +25,13 @@ import os
 from io import BytesIO
 from django.db.models import Q
 from django.utils.timezone import localtime, make_aware
+import tensorflow as tf
+from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
+import pytesseract
 
 
 
@@ -31,6 +40,7 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request , username=username , password=password)
+        print(username , password)
         if user is not None:
             login(request , user)
             return redirect('login')
@@ -38,3 +48,69 @@ def login(request):
             return render(request , 'login.html' , {'error': "Invalid Username or Password"})
     return render(request , 'login.html')
 
+
+def home(request):
+    if request.method == "POST":
+        return render(request , 'home.html')
+    
+
+def add_orphan_page(request):
+    return render(request , 'add_orphan.html')
+
+inception_model = InceptionV3(weights='imagenet')
+
+def is_white(color, threshold=200):
+    # Check if the color is close to pure white
+    return all(value >= threshold for value in color)
+
+def extract_dominant_color(img_array, k=3):
+    img_array_flat = img_array.reshape((-1, 3))
+
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(img_array_flat)
+
+    dominant_color_centers = kmeans.cluster_centers_.astype(int)
+
+    # Filter out white or near-white colors
+    dominant_colors = [tuple(color) for color in dominant_color_centers if not is_white(color)]
+
+    if not dominant_colors:
+        # If all dominant colors are white, use the second most dominant color
+        dominant_colors = [tuple(color) for color in dominant_color_centers[1:]]
+
+    return dominant_colors
+
+@csrf_exempt
+def classify_image(request):
+    if request.method == 'POST' and 'image' in request.FILES:
+        uploaded_image = request.FILES['image']
+        img = Image.open(uploaded_image)
+        img = img.resize((299, 299))
+        img_array = np.expand_dims(np.array(img), axis=0)
+        img_array = preprocess_input(img_array)
+
+        # Make predictions
+        predictions = inception_model.predict(img_array)
+        decoded_predictions = decode_predictions(predictions, top=1)[0]
+        classification_result = decoded_predictions[0][1]
+
+        # Extract dominant colors
+        dominant_colors = extract_dominant_color(np.array(img))
+
+        text = pytesseract.image_to_string(img)
+
+        print(f"Text:  {text}")
+
+        # Convert np.int64 to regular int for serialization
+        dominant_colors = [[int(value) for value in color] for color in dominant_colors]
+
+        result = {
+            'classification_result': classification_result,
+            'dominant_colors': dominant_colors,
+            "extracted_text": text,
+        }
+        return JsonResponse(result, encoder=DjangoJSONEncoder)
+
+    return JsonResponse({'error': 'Invalid request'})
+def index(request):
+    return render(request, 'add_orphan.html')
